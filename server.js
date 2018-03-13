b9e5c2df19bf55b6a6b5740c476f6a810269b683
Darwin Gonzalez Suarez <alu0100812794@ull.edu.es>
2018-03-13 12:01:54 +0000

    Cambiando el puerto de despliegue al 3000

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/practica2.html', function (req, res) {
 })
 
 
-var server = app.listen(8081, function () {
+var server = app.listen(3000, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
