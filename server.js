dc0e54d941fec47e62125cc4506560586f3db0a6
Darwin Gonzalez <alu0100812794@ull.edu.es>
2018-04-09 10:25:25 +0100

    Añadiendo modificación propuesta

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 
 var express = require('express');
+var url = require('url');
 var app = express();
 
+let rawUrl = 'http://localhost:3000/practica2.html/50';
+
+let parsedUrl = url.parse(rawUrl);  
+console.log(parsedUrl);
+
 app.use(express.static('public'));
+
 app.get('/practica2.html', function (req, res) {
    res.sendFile( __dirname + "/" + "practica2.html" );
 })
-
 
 var server = app.listen(3000, function () {
    var host = server.address().address
